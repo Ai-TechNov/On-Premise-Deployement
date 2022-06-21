2e848ced66b4a89e61cac050716c40ae4cbafae6
phanindravedula <phanindravedula@gmail.com>
2022-06-21 14:17:25 +0530

    Create main.py

---
+++ app2/flask-with-mysql/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+app = Flask(__name__, template_folder='/app2/templates/')
+
+
+app.config['MYSQL_HOST'] = '192.168.1.3'
+app.config['MYSQL_USER'] = 'app2'
+app.config['MYSQL_PASSWORD'] = 'P@$$w0rd'
+app.config['MYSQL_DB'] = 'app2'
+
+mysql = MySQL(app)
+
+
+@app.route('/app1', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUsers (FirstName, LastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
