64616154ab9a046177b4dcb034f3c7660b91a504
Vedula Phanindra <phanindravedula@gmail.com>
2022-09-28 08:33:40 +0530

    Update init.sql

--- app2/db/init.sql
+++ app2/db/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS appdata;
 CREATE TABLE IF NOT EXISTS appdata.MyUsers ( firstname VARCHAR(30) NOT NULL,  lastname VARCHAR(30) NOT NULL);
-CREATE USER 'app1'@'%' identified by 'P@$$w0rd';
+CREATE USER 'app1'@'192.168.180.130' identified by 'P@$$w0rd';
 grant all privileges on appdata.* to 'app1'@'192.168.180.130' ;
